2ba21058a4408453ff71c50c9709ea0675c87398
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-08-23 01:50:58 +0600

    Added more validation

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 
+// get player numbers dynamically to get player budget;
+let playerNumber = 0;
 const buttons = document.getElementsByClassName("button");
  for(const button of buttons) {
     button.addEventListener("click", () => {
@@ -11,12 +13,15 @@ const buttons = document.getElementsByClassName("button");
         const listItemArray = orderList.childNodes;
         if (listItemArray.length > 5) {
             button.setAttribute("disabled", true);
-            alert("Added 5 players. You can't select players more than 5. Thank you!")
+            alert("Sorry, You can't select players more than 5. Thank you!")
             return button.removeAttribute("disabled", true)
         }
-        button.style.backgroundColor = "#8080806b";
-        button.style.cursor = "none";
-        orderList.appendChild(listItem);
+         playerNumber += 1;
+         const playerNumberElement = document.getElementById("player-number");
+         playerNumberElement.innerText = playerNumber;
+         button.style.backgroundColor = "#8080806b";
+         button.style.cursor = "none";
+         orderList.appendChild(listItem);
       })
  }
 
@@ -39,16 +44,26 @@ const getTextElementValueById = (elementId) => {
  // get and calculate the player budget.
  document.getElementById("calc-player-budget").addEventListener("click", () => {
     const perPlayerBudget = getInputValueById("per-player-budget");
-    const totalPlayerBudget = perPlayerBudget * 5;
+    const totalPlayerBudget = perPlayerBudget * playerNumber;
+    const orderList  = document.getElementById("order-list");
+    const listItemArray = orderList.childNodes;
 
+    console.log(listItemArray);
      //Validate input field value
      const inputValue =  document.getElementById("per-player-budget");
       if(inputValue.value == ""){
-         alert("Player budget is emptry");
+         alert("Sorry, You don't have entered player budget.");
+         return;
+      }
+      if(listItemArray.length <=1) {
+         alert("Sorry, You don't select any players yet! Please select at least one player.");
          return;
       }
     const playerExpenseElement = document.getElementById("player-expense-total");
     playerExpenseElement.innerText = totalPlayerBudget;
+
+    // clear the input field's value;
+    inputValue.value = ""
  });
 
 
@@ -58,13 +73,23 @@ const getTextElementValueById = (elementId) => {
     const totalPlayerCost = getTextElementValueById("player-expense-total");
     const getTotalCost = totalPlayerCost + managerCost + coachCost;
 
+    // get the Player Expense element and check weather has value or not;
+    const playerExpenseElement = document.getElementById("player-expense-total");
+    if (playerExpenseElement.innerText <= 0) {
+      alert("Player expense amount is required.");
+      return;
+    }
     //Validate input field value
     const managerInput =  document.getElementById("manager-cost");
     const coachInput =  document.getElementById("coach-cost");
       if(managerInput.value == "" || coachInput.value == ""){
-         alert("Input field is emptry");
+         alert("Sorry, You don't have entered both amounts.");
          return;
       }
     const calculateTotalCostElement = document.getElementById("total-cost");
     calculateTotalCostElement.innerText = getTotalCost;
+
+    // clear the input field's value;
+      managerInput.value = "";
+      coachInput.value = "";
  })
